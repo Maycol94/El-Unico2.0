42128eda1369897d1be33b9d005893a2f372e46e
Maycol Duque <maycolduque1994@gmail.com>
2020-11-20 21:35:01 -0600

    boton

--- El_Unico_Grupo3/El_Unico_Grupo3/RegistroCliente.Designer.cs
+++ El_Unico_Grupo3/El_Unico_Grupo3/RegistroCliente.Designer.cs
@@ -30,6 +30,7 @@ namespace El_Unico_Grupo3
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,21 @@ namespace El_Unico_Grupo3
             this.label1.TabIndex = 0;
             this.label1.Text = "REgistro cliente";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // RegistroCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "RegistroCliente";
             this.Text = "RegistroCliente";
@@ -57,5 +68,6 @@ namespace El_Unico_Grupo3
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
