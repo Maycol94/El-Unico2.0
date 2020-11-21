d0218e64e300c291d6450db595d337f81df52845
Maycol Duque <maycolduque1994@gmail.com>
2020-11-21 02:36:05 -0600

    Modifique el registro de facturas

--- El_Unico_Grupo3/El_Unico_Grupo3/RegistroFactura.Designer.cs
+++ El_Unico_Grupo3/El_Unico_Grupo3/RegistroFactura.Designer.cs
@@ -29,12 +29,41 @@ namespace El_Unico_Grupo3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroFactura));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 26F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(522, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro Factura";
+            // 
+            // RegistroFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "RegistroFactura";
             this.Text = "RegistroFactura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
