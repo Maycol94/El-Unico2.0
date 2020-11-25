d38cb0e765c8d5b2975f3b975ba76f97d6bd25ed
RENE HERNANDEZ <jrh_man23@hotmail.com>
2020-11-25 09:41:34 -0600

    actualizzacion

--- El_Unico_Grupo3/El_Unico_Grupo3/Form2.cs
+++ El_Unico_Grupo3/El_Unico_Grupo3/Form2.cs
@@ -16,5 +16,10 @@ namespace El_Unico_Grupo3
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
